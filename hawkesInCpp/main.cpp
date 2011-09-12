0ab42160a82d2778d44c43dd4609f0344829ddec
Siva Kumar Gorantla <sivaiitm@Sivas-MacBook-Pro.local>
2011-09-11 18:15:59 -0700

    Initial Commit

---
+++ hawkesInCpp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  hawkesInCpp
+//
+//  Created by Siva Kumar Gorantla on 9/11/11.
+//  Copyright 2011 University of Illinois. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
